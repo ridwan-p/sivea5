8e546c7cd0bfc9ef095b167cb1349294e08b24c9
Bunny Pro Developer <mbarrid.s@students.amikom.ac.id>
2016-05-07 13:03:16 +0700

    add graham-campbell/exception provider

--- config/app.php
+++ config/app.php
@@ -147,6 +147,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        
+        GrahamCampbell\Exceptions\ExceptionsServiceProvider::class,
 
         /*
          * Application Service Providers...
